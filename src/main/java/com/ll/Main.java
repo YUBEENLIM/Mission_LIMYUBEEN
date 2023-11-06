e72dc8be9dad0c63f0af3928826ef5c784b91ecd
YUBEENLIM <bgs0782@gmail.com>
2023-11-06 11:33:45 +0900

    4단계

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -6,7 +6,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
-        while(true) {
+        int n = 0;
+
+        while (true) {
             System.out.print("명령) ");
             String a = scanner.nextLine();
             if (a.equals("등록")) {
@@ -14,11 +16,11 @@ public class Main {
                 String quote = scanner.nextLine();
                 System.out.print("작가 : ");
                 String authorName = scanner.nextLine();
+                n++;
                 System.out.println("명언 : " + quote);
                 System.out.println("작가 : " + authorName);
-                System.out.println("1번 명언이 등록되었습니다.");
-            }
-            else if(a.equals("종료")) {
+                System.out.println(n + "번 명언이 등록되었습니다.");
+            } else if (a.equals("종료")) {
                 break;
             }
         }
