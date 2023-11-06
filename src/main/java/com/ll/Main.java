8821e94f918788928ac72fd23641bf80ca6e45aa
YUBEENLIM <bgs0782@gmail.com>
2023-11-06 15:02:57 +0900

    5단계

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,29 +1,49 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        List<Quote> quotes = new ArrayList<>();
         System.out.println("== 명언 앱 ==");
         Scanner scanner = new Scanner(System.in);
-        int n = 0;
+        int id = 0;
+
 
         while (true) {
             System.out.print("명령) ");
-            String a = scanner.nextLine();
-            if (a.equals("등록")) {
+            String cmd = scanner.nextLine();
+            if (cmd.equals("등록")) {
                 System.out.print("명언 : ");
-                String quote = scanner.nextLine();
+                String content = scanner.nextLine();
                 System.out.print("작가 : ");
                 String authorName = scanner.nextLine();
-                n++;
-                System.out.println("명언 : " + quote);
+                id++;
+                System.out.println("명언 : " + content);
                 System.out.println("작가 : " + authorName);
-                System.out.println(n + "번 명언이 등록되었습니다.");
-            } else if (a.equals("종료")) {
-                break;
+                System.out.println(id + "번 명언이 등록되었습니다.");
+                Quote quote = new Quote(id, content, authorName);
+                quotes.add(quote);
             }
+                else if (cmd.equals("목록")) {
+
+
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("---------------------");
+                    for (int i = quotes.size() - 1; i >= 0; i--) {
+                        Quote quote = quotes.get(i);
+                        System.out.printf("%d / %s / %s\n", quote.id, quote.authorName, quote.content);
+                    }
+                }
+                else if (cmd.equals("종료")) {
+                    break;
+                }
+            }
+            scanner.close();
         }
-        scanner.close();
     }
-}
+
+
+
---
+++ src/main/java/com/ll/Quote.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+class Quote {
+    int id;
+    String content;
+    String authorName;
+
+    public Quote(int id, String authorName, String content) {
+        this.id = id;
+        this.content = content;
+        this.authorName = authorName;
+    }
+}
