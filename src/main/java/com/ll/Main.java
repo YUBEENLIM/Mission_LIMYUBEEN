c1081174ebe2650c12919dfc59a28bdffe6baa65
YUBEENLIM <bgs0782@gmail.com>
2023-11-06 16:16:29 +0900

    7단계

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -46,8 +46,12 @@ public class Main {
                         quotes.remove(Deleteid - 1);
                         System.out.println(Deleteid + "번 명언이 삭제되었습니다.");
                     }
-
-
+                    else if(quotes.isEmpty()){
+                        System.out.println(Deleteid + "번 명언은 존재하지 않습니다.");
+                    }
+                    else{
+                        System.out.println(Deleteid + "번 명언은 존재하지 않습니다.");
+                    }
 
 
             }
