64ea4532fe6d063ce1a63b5086b6b2f7331da76b
YUBEENLIM <bgs0782@gmail.com>
2023-11-06 11:17:00 +0900

    2단계

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,9 +5,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
         Scanner scanner = new Scanner(System.in);
-        String a = scanner.nextLine();
+        while(true) {
+            System.out.print("명령) ");
+            String a = scanner.nextLine();
+            if (a.equals("등록")) {
+                System.out.print("명언 : ");
+                String quote = scanner.nextLine();
+                System.out.print("작가 : ");
+                String authorName = scanner.nextLine();
+                System.out.println("명언 : " + quote);
+                System.out.println("작가 : " + authorName);
+            }
+            else if(a.equals("종료")) {
+                break;
+            }
+        }
         scanner.close();
     }
 }
